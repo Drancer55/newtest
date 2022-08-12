3a46ab640f01dc207459ce0c162a8648a308df43
Oscar Ruiz Antonio <86693936+Drancer55@users.noreply.github.com>
2022-08-12 17:53:11 -0500

    Preparation of the development environment

---
+++ test/src/login/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './login.css';
+
+
+
+const Login = () => {
+    return (
+        <div className="login">
+            <p>Login</p>
+        </div>
+    )
+};
+
+export default Login;
---
+++ test/src/point/Point.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './point.css';
+import { Card } from "react-bootstrap";
+
+const Point = () => {
+    return (
+        <div className="point">
+            <h1>Logo</h1>
+        </div>
+    )
+};
+
+export default Point;
